1e92ff20d2815bb998d4e9cd2757707b94e1d2c7
satish4701 <s.b.rathod040701@gmail.com>
2023-09-28 21:53:09 +0530

    updated the form

--- mail/contact.php
+++ mail/contact.php
@@ -9,12 +9,18 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "s.b.rathod040701@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body, $header))
+if(mail($to, $subject, $body, $header)){
+  header('Location: thank-you.html');
+//  print_r('Thanks')
+}else {
   http_response_code(500);
+  echo "Error: Unable to send the email.";
+}
+ 
 ?>
